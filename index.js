140c17780ebac170cad8dcbacc6e09c255bb738e
DrMoraschi <68395930+DrMoraschi@users.noreply.github.com>
2021-01-06 22:26:59 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,286 +1,258 @@
-var mineflayer = require('mineflayer');
-var notifier = require('node-notifier');
-var bloodhoundPlugin = require('mineflayer-bloodhound')(mineflayer);
-var { pathfinder, Movements } = require('mineflayer-pathfinder');
+console.clear()
+
+const mineflayer = require('mineflayer');
+const notifier = require('node-notifier');
+const bloodhoundPlugin = require('mineflayer-bloodhound')(mineflayer);
+const { pathfinder, Movements } = require('mineflayer-pathfinder');
 const autoeat = require('mineflayer-auto-eat');
-var { GoalFollow } = require('mineflayer-pathfinder').goals
-var botConfig = require('./config.json');
+const { GoalFollow } = require('mineflayer-pathfinder').goals
+const { server, botOptions, announcements, misc, timeouts } = require('./config.json');
 const Discord = require('discord.js');
-var dsURL = botConfig.webhook.webhookUrl.split('/');
+const chalk = require('chalk');
 
-if (botConfig.webhook.sendMessage === 'true') {
-  if (botConfig.webhook.webhookUrl === '') {
-    console.log('\n   \x1b[34m<DISCORD> Please specify a Discord WebHook Url\x1b[0m');
-    process.exit(1);
-  } else if (botConfig.webhook.userIDToPing === '') {
-    console.log('\n   \x1b[34m<DISCORD> Please specify a user ID to ping\x1b[0m');
-    process.exit(1);
-  } else {
-    global.hook = new Discord.WebhookClient(dsURL[5], dsURL[6]);
-  };
-};
+startBot();
 
-startBot()
-
-function startBot() {
-  var bot = mineflayer.createBot({
-    host: botConfig.host,
-    port: parseInt(botConfig.port),
-    username: botConfig.username ? botConfig.username : 'AFKBot',
-    password: botConfig.password,
-    hideErrors: true
-  })
-
-  //LOAD PLUGIN, FUNCTIONS
-  bot.loadPlugin(pathfinder);
-
-  bot.loadPlugin(autoeat);
-  
-  var incomingnotification = botConfig.windowsAnnouncements
-  var owner = botConfig.owner
-  var attackmob = botConfig.attackMobs
-  var sendToDS = botConfig.webhook.sendMessage
-
-  bot.on('login', () => {
-    bot.loadPlugin(require('mineflayer-dashboard'));
-  });
-
-  bot.once('spawn', () => {
-    bot.autoEat.options = {
-      priority: `${botConfig.autoEat.priority}`,
-      startAt: botConfig.autoEat.startAt,
-      bannedFood: [],
-    };
-
-    bot.setControlState('jump', true)
-
-    var playersList = Object.keys(bot.players).join(", ");
-    bot.dashboard.log(`\x1b[36m<WORLD> Online players: ${playersList}`);
-    if (sendToDS === 'true') {
-      bot.dashboard.log(`\x1b[34m<DISCORD> Webhook found`+'\x1b[0m');
-      hook.send(`**Online players: ${playersList}**`);
-    };
-  });
-
-
-  //LOOKING
-  bot.once('login', () => {
-    setInterval(() => {
-        const entity = bot.nearestEntity();
-          if (entity !== null) {
-            if (entity.type === 'player') {
-            bot.lookAt(entity.position.offset(0, 1.6, 0));
-            } else if (entity.type === 'mob') {
-              bot.lookAt(entity.position);
-            };
-          };
-        }, 50);
-    bot.on('kicked', () => {
-      return
-    });
-  });
-
-
-
-  //CHAT
-  bot.on('whisper', (username, message, rawMessage) => {
-      if (message === 'follow me') {
-        if (username === owner) {
-          bot.whisper(username, 'On my way');
-      }} else if (message === 'stop') {
-          null
-      } else {
-          if (message !== 'follow me' && message !== 'stop') {
-            if (username !== bot.username) {
-                bot.whisper(username, 'Sorry, I am an AFK Bot');
-                bot.dashboard.log('\x1b[32m<STATUS> Correctly whispered that I am a bot','\x1b[0m');
-              };
-          };
-      };
-          if (incomingnotification === 'true') {
-            if (username !== bot.username) {
-              if (username !== 'You') {
-                if (message !== 'follow me' && message !== 'stop') {
-                  if (message !== 'Sorry, I am an AFK Bot') {
-                    notifier.notify({
-                      title: 'Whisper Message',
-                      message: ('You have a new message'),
-                      icon: 'projectlogo.jpg'
-                    });
-                  };
-                };
-              };
-            };
-          };
-  });
-
-  bot.on('message', (msg) => {
-    if (sendToDS === 'true') {
-      hook.send(msg.toString());
-    };
-  });
-
-
-
-  //KICK & ERRORS
-  bot.on('kicked', (reason) => {
-    reason = JSON.parse(reason);
-    bot.dashboard.log('\x1b[32m<STATUS>\x1b[0m \x1b[31mI got kicked.\x1b[0m');
-    if (incomingnotification === 'true') {
-      notifier.notify({
-        title: 'Event Message',
-        message: ('I got kicked!'),
-        icon: 'projectlogo.jpg'
-      });
-    };
-
-    if (sendToDS === 'true') {
-      hook.send(`<@${botConfig.webhook.userIDToPing}> **I got kicked!**`);
-    };
-
-    setTimeout(() => {
-      startBot();
-    }, 10000);
-
-    if (!reason.extra) return
-
-    if (reason.extra[0].text.includes('banned') === true) {
-      bot.dashboard.log('\x1b[32m<STATUS>\x1b[0m \x1b[31mI got banned. Exiting in 10 seconds...');
-
-      if (sendToDS === 'true') {
-        hook.send(`<@${botConfig.webhook.userIDToPing}> **I got banned! Exiting in 10 seconds...**`);
-      };
-      setTimeout(() => {
-        process.exit(1);
-      }, 10000);
-    };
-  });
-  
-
-  
-  //EVENTS
-  bot.on('death', () => {
-    bot.dashboard.log(`\x1b[32m<STATUS>\x1b[0m \x1b[31mI died`+'\x1b[0m');
-    if (incomingnotification === 'true') {
-      notifier.notify({
-        title: 'Event Message',
-        message: ('I died!'),
-        icon: 'projectlogo.jpg'
-      });
-    };
-
-    if (sendToDS === 'true') {
-      hook.send(`<@${botConfig.webhook.userIDToPing}> **I died!**`);
-    };
-  });
-
-  bot.once('spawn', () => {
-    bot.dashboard.log(`\x1b[32m<STATUS> Spawned at X: ${Math.floor(bot.entity.position.x)} Y: ${Math.floor(bot.entity.position.y)} Z: ${Math.floor(bot.entity.position.z)}`+'\x1b[0m');
-  });
-
-  bot.on('respawn', () => {
-    bot.dashboard.log(`\x1b[32m<STATUS> Respawned at X: ${Math.floor(bot.entity.position.x)} Y: ${Math.floor(bot.entity.position.y)} Z: ${Math.floor(bot.entity.position.z)}`+'\x1b[0m');
-  });
-
-  bot.on('login', () => {
-    bot.dashboard.log('\x1b[32m<STATUS> Logged in'+'\x1b[0m');
-  });
-
-  bot.once('health', () => {
-    bot.dashboard.log(`\x1b[32m<STATUS> I have ${Math.floor(bot.health)} health.`+'\x1b[0m');
-
-    if (bot.health <= 5)
-      bot.dashboard.log(`\x1b[32m<STATUS> \x1b[33mMy remaining health is ${bot.health}`+'\x1b[0m');
-  });
-
-  bot.on('health', () => {
-    if (bot.food === 20) {
-      bot.autoEat.disable();
-    } else {
-      bot.autoEat.enable();
-    };
-  });
-
-
-
-  //WORLD
-  bot.once('time', () => {
-    setTimeout(function() {
-    bot.dashboard.log(`\x1b[36m<WORLD> \x1b[36mCurrent time: `+Math.abs(bot.time.timeOfDay)+'\x1b[0m');
-  }, 1000)});
-
-
-
-  //PATHFIND
-  bot.once('spawn', () => {
+function startBot()
+{
+    if (announcements.discordBot.sendMessage === true) {
+        if (announcements.discordBot.webchannelUrl === '') {
     
-    const mcData = require('minecraft-data')(bot.version);
-
-    const defaultMove = new Movements(bot, mcData);
-    defaultMove.allowFreeMotion = true
-
-    bot.on('whisper', (username, message) => {
-      if (username === bot.username) return
-
-      const target = bot.players[username].entity
-      if (username === owner) {
-        if (message === 'follow me') {
-          bot.pathfinder.setMovements(defaultMove);
-          bot.pathfinder.setGoal(new GoalFollow(target, 2), true);
-        } else if (message === 'stop') {
-          bot.pathfinder.setGoal(null);
-        };
-      };
-    });
-  });
-
-
-
-  //AFK
-  bot.once('spawn', () => {
-    bot.dashboard.log('\x1b[32m<STATUS> Starting anti-AFK-kick sequence'+'\x1b[0m');
-    setInterval(() => {
-      setTimeout(() => {
-       bot.setControlState('jump', false);
-      }, 100);
-      bot.setControlState('jump', true);
-    }, 600000);
-  });
-
-  bot.once('spawn', () => {
-    if (attackmob === 'true' ) {
-      setInterval(() => {
-          const mobFilter = e => e.type === 'mob' && e.position.distanceTo(bot.entity.position) < 8 &&
-                e.mobType !== 'Armor Stand'
-          const mob = bot.nearestEntity(mobFilter);
-
-          if (!mob) return
-
-          const pos = mob.position
-          bot.lookAt(pos, true, () => {
-        bot.setControlState('jump', true);
-        setTimeout(() => {
-          bot.attack(mob);
-        }, 500);
-        bot.setControlState('jump', false);
-          });
-      }, 1000);
-  };
-  });
-
-
-
-  //BLOODHOUND
-  bloodhoundPlugin(bot);
-
-  bot.bloodhound.yaw_correlation_enabled = true
-
-  bot.on('onCorrelateAttack', function (attacker,victim,weapon) {
-    if (bot.username === victim.username) {
-      if (weapon) {
-          bot.dashboard.log(`\x1b[32m<STATUS>\x1b[33m Got hurt by ${attacker.displayName || attacker.username} with a/an ${weapon.displayName}\x1b[0m`);
+            console.log(chalk.blueBright('\n   <DISCORD> Please specify a Discord Webchannel Url'));
+            process.exit(1);
         } else {
-          bot.dashboard.log(`\x1b[32m<STATUS>\x1b[33m Got hurt by ${attacker.displayName || attacker.username}\x1b[0m`);
+    
+            global.client = new Discord.Client();
+            client.login(announcements.discordBot.token)
         };
-  }});
+    };
+
+    const bot = mineflayer.createBot({
+        host: server.host,
+        username: botOptions.username ? botOptions.username : 'AFKBot',
+        password: botOptions.password,
+        hideErrors: true
+    });
+
+    bloodhoundPlugin(bot);
+    bot.bloodhound.yaw_correlation_enabled = true
+
+    bot.loadPlugin(pathfinder);
+
+    bot.loadPlugin(autoeat);
+
+    var pingOn
+    if (!announcements.discordBot.userIDToPing) {
+        pingOn = false
+    } else {
+        pingOn = true
+    };
+
+    const windowsNotification = announcements.windowsAnnouncements
+    const botOwner = misc.owner
+    const attackMob = misc.attackMob
+    const sendToDS = announcements.discordBot.sendMessage
+
+    bot.once('spawn', () => {
+        const channel = client.channels.cache.get(announcements.discordBot.channelID)
+        
+        channel.send('**Spawned!**');
+
+        const mcData = require('minecraft-data')(bot.version);
+        const defaultMove = new Movements(bot, mcData);
+        defaultMove.allowFreeMotion = true
+        bot.pathfinder.setMovements(defaultMove);
+
+        lookNearEntity();
+        if (attackMob === true) attackMobs();
+
+        bot.autoEat.options = {
+            priority: `${misc.autoEat.priority}`,
+            startAt: misc.autoEat.startAt,
+            bannedFood: [],
+        };
+
+        var playersList = Object.keys(bot.players).join(", ");
+
+        if (sendToDS === true) console.log(chalk.blueBright(` <DISCORD> Token found`));
+        console.log(chalk.blueBright(` <WORLD> Online players: `)+playersList);
+        console.log(chalk.blueBright(` <WORLD> Current time: `)+Math.abs(bot.time.timeOfDay));
+
+        console.log(chalk.greenBright(` <STATUS> Spawned at x: ${chalk.white(Math.round(bot.entity.position.x))} y: ${chalk.white(Math.round(bot.entity.position.y))} z: ${chalk.white(Math.round(bot.entity.position.z))}`));
+        
+        client.on('message', (message) => {
+            if (message.author.id === client.user.id) return
+            if (message.channel.id !== announcements.discordBot.channelID) return
+            if (message.content.startsWith(`${announcements.discordBot.prefix}`) === true) {
+                const commandSplit = message.content.replace(`${announcements.discordBot.prefix}`, '')
+
+                bot.chat(commandSplit)
+            };
+        });
+
+        bot.once('health', () => {
+            if (bot.health <= 5) {
+                console.log(chalk.yellowBright(` <STATUS> I have ${Math.floor(bot.health)} health.`));
+            } else {
+                console.log(chalk.greenBright(` <STATUS> I have ${Math.floor(bot.health)} health.`));
+            };
+        });
+
+        bot.on('health', () => {
+            if (bot.food === 20) {
+                bot.autoEat.disable();
+            } else {
+                bot.autoEat.enable();
+            };
+        });
+
+        bot.on('message', (msg) => {
+            if (sendToDS === true) channel.send(msg.toString());
+
+            console.log(` ${msg.toAnsi()}`)
+        });
+
+        bot.on('whisper', (username, message) => {
+            if (username === bot.username) return
+            const playerToFollow = bot.players[username].entity
+
+            if (username === botOwner) {
+                switch (message) {
+                    case `follow me`:
+                        followbotOwner(playerToFollow, username);
+                        break
+                    case `stop`:
+                        bot.pathfinder.setGoal(null);
+                        break
+                };
+            } else if (username !== botOwner) {
+                bot.whisper(username, 'Sorry, I am an AFK Bot');
+                console.log(chalk.greenBright(' <STATUS> Whispered that I am a bot'));
+
+                if (windowsNotification === true) notifierSend('Whisper Message', 'You have a new message');
+            };
+        });
+
+        bot.on('kicked', (reason) => {
+            const reasonKicked = JSON.parse(reason);
+
+            console.log(chalk.redBright(' <STATUS> I got kicked.'));
+            if (windowsNotification === true) notifierSend('Event Message', 'I got kicked!');
+        
+            if (sendToDS === true) {
+                if (pingOn === true) {
+                    channel.send(`<@${announcements.discordBot.userIDToPing}> **I got kicked!**`);
+                } else {
+                    channel.send(`**I got kicked!**`);
+                };
+            }; 
+        
+            setTimeout(() => {
+                startBot();
+            }, timeouts.onKicked);
+        
+            if (!reasonKicked.extra) return
+            if (reasonKicked.extra[0].text.includes('banned') === true) {
+                console.log(chalk.redBright(' <STATUS> I got banned. Exiting in 5 seconds...'));
+        
+                if (sendToDS === true) {
+                    if (pingOn === true) {
+                        channel.send(`<@${announcements.discordBot.userIDToPing}> **I got banned! Exiting in 10 seconds...**`);
+                    } else {
+                        channel.send(`**I got banned! Exiting in 10 seconds...**`);
+                    };
+                };
+
+                setTimeout(() => {
+                    process.exit(1);
+                }, 5000);
+            };
+        });
+
+        bot.on('death', () => {
+            console.log(chalk.redBright(` <STATUS> I died!`));
+            if (windowsNotification === true) notifierSend('Event Message', 'I died!');
+        
+            if (sendToDS === true) {
+                if (pingOn === true) {
+                    channel.send(`<@${announcements.discordBot.userIDToPing}> **I died!**`);
+                } else {
+                    channel.send(`**I died!**`);
+                };
+            };
+        });
+
+        bot.on('respawn', () => {
+            console.log(chalk.greenBright(` <STATUS> Respawned at x: ${chalk.white(Math.round(bot.entity.position.x))} y: ${chalk.white(Math.round(bot.entity.position.y))} z: ${chalk.white(Math.round(bot.entity.position.z))}`));
+        });
+
+        bot.on('onCorrelateAttack', function (attacker,victim,weapon) {
+            if (bot.username === victim.username) {
+                if (weapon) {
+                    console.log(chalk.yellowBright(` <STATUS> Got hurt by ${chalk.whiteBright(attacker.displayName || attacker.username)} with a/an ${chalk.whiteBright(weapon.displayName)}`));
+                } else {
+                    console.log(chalk.yellowBright(` <STATUS> Got hurt by ${chalk.whiteBright(attacker.displayName || attacker.username)}`));
+                };
+            };
+        });
+
+        setInterval(() => {
+            setTimeout(() => {
+                bot.setControlState('jump', false);
+            }, 100);
+                bot.setControlState('jump', true);
+        }, timeouts.antiAFK);
+
+        function followbotOwner(playerToFollow, username)
+        {
+            bot.whisper(username, 'On my way');
+            bot.pathfinder.setGoal(new GoalFollow(playerToFollow, 2), true);
+        };
+
+        function lookNearEntity()
+        {
+          setInterval(() => {
+            const entity = bot.nearestEntity();
+                if (entity !== null) {
+                    if (entity.type === 'player') {
+                        bot.lookAt(entity.position.offset(0, 1.6, 0));
+                    } else if (entity.type === 'mob') {
+                        bot.lookAt(entity.position);
+                    };
+                };
+            }, 50);
+        };
+
+        function attackMobs()
+        {
+            bot.on('entityMoved', (entity) => {
+                if (entity.type === 'mob' && entity.position.distanceTo(bot.entity.position) < 8 && entity.mobType !== 'Armor Stand') {
+                    const mobFilter = e => e.type === 'mob' && e.position.distanceTo(bot.entity.position) < 8 && e.mobType !== 'Armor Stand'
+
+                    const mob = bot.nearestEntity(mobFilter);
+          
+                    if (!mob) return
+                    const pos = mob.position
+    
+                    bot.lookAt(pos, true, () => {
+                        bot.setControlState('jump', true);
+    
+                        setTimeout(() => {
+                            bot.attack(mob);
+                        }, 500);
+    
+                        bot.setControlState('jump', false);
+                    });
+                };
+            });
+        };
+
+        function notifierSend(title, message)
+        {
+            notifier.notify({
+                title: `${title}`,
+                message: (`${message}`),
+                icon: 'projectlogo.jpg'
+            });
+        };
+    });
 };
