a475e76f1c8f3c202b41883cb54f5841dee781bb
DrMoraschi <68395930+DrMoraschi@users.noreply.github.com>
2021-03-25 22:21:44 +0100

    Add files via upload

--- src/Discord.js
+++ src/Discord.js
@@ -4,12 +4,18 @@ const { logToFile } = require('../index');
 
 logToFile('<src/Discord.js> Trying to start Discord Bot', dir);
 
-const notEnoughCreds = 'Please specify a Discord Bot Token, a Discord Server ID and a Discord Channel ID';
+const notEnoughCreds = `ERROR: Please specify a Discord Bot Token, a Discord Server ID and a Discord Channel ID`;
+const notCorrectIds = `ERROR: server ID or channel ID is incorrect, or I cannot access them, please check again\n`;
 const neededCreds = config.discord.token || config.discord['server-id'] || config.discord['channel-id'];
 if (!neededCreds) return console.log(notEnoughCreds);
 
 const client = new Discord.Client();
-client.login(config.discord.token);
+client.login(config.discord.token)
+.catch(error =>
+{
+    console.log(`Error trying to initialize Discord Bot: ${error} Process will exit.`);
+    process.exit(0);
+});
 
 if (config.debug) log(`<src/Discord.js> creating bot`);
 client.on('ready', () =>
@@ -20,6 +26,7 @@ client.on('ready', () =>
         if (config.debug) log(`<src/Discord.js> retrieving channel`);
         const channel = client.channels.cache.get(config.discord['channel-id']);
         const guild = client.guilds.cache.get(config.discord['server-id']);
+        if (!guild || !channel) return console.log(notCorrectIds);
         if (!guild.members.cache.get(client.user.id).hasPermission('ADMINISTRATOR')) return errEmbed(`Missing permissions`, `- Make sure I have Administrator permissions`);
     
         function errEmbed(err, solutionList)
